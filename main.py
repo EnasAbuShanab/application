c411fe93ae6c5512426e2ea22ab96b1b8e80e90d
WEnas98 <104684526+EnasAbuShanab@users.noreply.github.com>
2022-09-30 19:30:37 +0000

    ctime

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ while (x<9):
   print (" the count is:\n",x) # \n is to start a new line before it
   x=x+1
 print ("Good luck")
+
+
+
+import webbrowser
+import time
+start_time= time.ctime()
+print (start_time)
+
+
