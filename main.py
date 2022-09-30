854ebd40b0f0b20372f22a79b3f802680e8b1ca7
WEnas98 <104684526+EnasAbuShanab@users.noreply.github.com>
2022-09-30 19:07:13 +0000

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print ("hello")
+#this is a counting system
 x= 0
 while (x<9):
-  print (" the count is:",x)
+  print (" the count is:\n",x) # \n is to start a new line before it
   x=x+1
 print ("Good luck")
