74cf8f53ef962bc67a94f7e9a788a2b090308492
WEnas98 <104684526+EnasAbuShanab@users.noreply.github.com>
2022-10-02 18:33:38 +0000

    input

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 print ("hello")
 #this is a counting system
+import webbrowser
+import time
 x= 0
 while (x<9):
   print (" the count is:\n",x) # \n is to start a new line before it
   x=x+1
 print ("Good luck")
 
+print("\n")
+
 e,f,g= 1,3.5, "abc"
 print(e,f,g)
 
+print("wael \n al \t qawasmi")
 
 x={1,3,6}
 x.add (9)
 print (x)
 
+first_name= input("enter first name: ")
+second_name= input("enter second name: ")
+print(first_name+" " +second_name) 
 
-import webbrowser
-import time
+
+
+
 start_time= time.ctime()
 print (start_time)
 
@@ -28,4 +37,3 @@ while start<= target:
 start= start+1
 
 
-  
